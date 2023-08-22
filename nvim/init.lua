08ba121249682365c6af8ae227aa32e57b0d9e07
alekay2200 <alejandropalomino22@gmail.com>
2023-08-22 12:13:33 +0200

    NVchad config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,21 +1,21 @@
--- Load configuration from this file
-local vimrc = vim.fn.stdpath("config") .. "/vimrc.vim"
-vim.cmd.source(vimrc)
+require "core"
 
--- Load all plugins
--- require("plugins")
-require("plugins.lsp_config")
-require("plugins.completions")
+local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
 
--- Rust --
-local rt = require("rust-tools")
-rt.setup({
-  server = {
-    on_attach = function(_, bufnr)
-      -- Hover actions
-      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-      -- Code action groups
-      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-    end,
-  },
-})
+if custom_init_path then
+  dofile(custom_init_path)
+end
+
+require("core.utils").load_mappings()
+
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+-- bootstrap lazy.nvim!
+if not vim.loop.fs_stat(lazypath) then
+  require("core.bootstrap").gen_chadrc_template()
+  require("core.bootstrap").lazy(lazypath)
+end
+
+dofile(vim.g.base46_cache .. "defaults")
+vim.opt.rtp:prepend(lazypath)
+require "plugins"
--- nvim/plugins/init.lua
+++
@@ -1,4 +0,0 @@
-require("./lsp_config")
--- require("nvim-tree")
--- require("nvim-web-devicons")
-require("./completions")
