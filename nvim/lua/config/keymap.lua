11e3085dabfe4d67bb8d940d1432e5464c132572
alekay2200 <alejandropalomino22@gmail.com>
2024-03-08 09:56:35 +0100

    Added movement between windows

--- nvim/lua/config/keymap.lua
+++ nvim/lua/config/keymap.lua
@@ -1,5 +1,11 @@
 local keymap = vim.keymap
 
+-- Window movement
+keymap.set("n", "<C-h>", "<C-w>h", { noremap = false, silent = true })
+keymap.set("n", "<C-j>", "<C-w>j", { noremap = false, silent = true })
+keymap.set("n", "<C-k>", "<C-w>k", { noremap = false, silent = true })
+keymap.set("n", "<C-l>", "<C-w>l", { noremap = false, silent = true })
+
 -- Movement
 keymap.set("v", '<S-k>', ':move \'<-2<CR>gv-gv', { noremap = true, silent = true })
 keymap.set("v", '<S-j>', ':move \'>+1<CR>gv-gv', { noremap = true, silent = true })
@@ -9,4 +15,4 @@ keymap.set("n", "<leader>d", ":NvimTreeToggle<CR>", { noremap = true, silent = t
 
 -- Comments
 -- Not working I dont know why :)
-keymap.set("n", "<C-/>", "gcc", { noremap = false })
+keymap.set("n", "<C-/>", "gcc", { noremap = false, silent = true})
