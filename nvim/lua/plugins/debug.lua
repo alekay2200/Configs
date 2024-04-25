1c8f89ccf7eb8138bb03b22d064635ad2d255d61
alejandro.palomino <alejandro.palomino@vodafone.com>
2024-04-25 11:43:39 +0200

    Added dap-ui and dap for go

--- nvim/lua/plugins/debug.lua
+++ nvim/lua/plugins/debug.lua
@@ -1,5 +1,32 @@
--- debug adapter protocol
-  return {
-   "mfussenegger/nvim-dap",
-   lazy = false,
+-- DAP (debug adapter protocol)
+
+
+return {
+ "rcarriga/nvim-dap-ui",
+  dependencies = {
+    "mfussenegger/nvim-dap",
+    "nvim-neotest/nvim-nio",
+    "leoluz/nvim-dap-go" -- Debbuger for GO
+  },
+  lazy = false,
+  config = function()
+    require("dap-go").setup()
+
+    local dap, dapui = require("dap"), require("dapui")
+
+    dapui.setup()
+
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+  end
 }
